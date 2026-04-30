07fff7a3912e52b5b7d92d01eebcbae24eb331aa
Pratyay Saha <pratyaysaha@Pratyays-MacBook-Air.local>
2026-05-01 02:47:58 +0530

    Addition of test notification feature

--- src/routes/notification.route.js
+++ src/routes/notification.route.js
@@ -1,34 +1,46 @@
 const express = require("express");
-const notificationService = require("../services/notification.service")
+const notificationService = require("../services/notification.service");
 
 const router = express.Router();
 
 router.post("/subscribe", async (req, res) => {
     try {
         const subscription = req.body;
-        console.log("Sub ", subscription)
         const result = notificationService.subscribePushNotification(subscription);
-        res.json(result)
+        res.json(result);
     } catch (ex) {
-        console.error(ex)
+        console.error(ex);
         res.status(500).json({
             success: false,
             message: ex.message
-        })
+        });
     }
 });
 
 router.get("/subscriptions", async (req, res) => {
     try {
         const subscriptions = notificationService.getAllSubscriptions();
-        res.json(subscriptions)
+        res.json(subscriptions);
     } catch (ex) {
         console.error(ex);
         res.status(500).json({
             success: false,
             message: ex.message
-        })
+        });
     }
-})
+});
+
+router.post("/test", async (req, res) => {
+    try {
+        const result = await notificationService.sendTestNotification(req.body);
+        res.json(result);
+    } catch (ex) {
+        console.error(ex);
+        res.status(500).json({
+            success: false,
+            message: ex.message
+        });
+    }
+});
 
 module.exports = router;
